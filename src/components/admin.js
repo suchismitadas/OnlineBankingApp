3e6d14969f087b66b4e879904f4b2da72c6f3124
unknown <suchis.das123@gmail.com>
2023-08-24 11:52:35 +0530

    made admin dashboard

--- src/components/admin.js
+++ src/components/admin.js
@@ -3,26 +3,96 @@ import ApprovedList from './admincomponents/approvedlist';
 import PendingList from './admincomponents/pending';
 import CustomerList from './admincomponents/viewall';
 import { Button } from "react-bootstrap";
-import { useState } from "react";
+import { useState ,useEffect} from "react";
+import axios from 'axios';
+import {Table} from 'react-bootstrap';
+import {Sheet } from '@mui/joy';
 
 const Admin=()=>{
-    const [typeList, setTypeList] = useState(0);
+    const [alluser,setAlluser] = useState([]);
+     
+
+    useEffect(()=> {
+        console.log("This is running");
+
+        const getData = async () => {
+            const loggedInID = localStorage.getItem('id');
+            const token = localStorage.getItem('token');
+            const header = {
+            
+                Authorization: `Bearer ${token}`
+                };
+              try {  
+             const response = await axios.get(`http://localhost:3000/customer-details`, {headers: header});
+    
+            console.log(response.data);
+            setAlluser(response.data);   
+              } catch(error) {
+                console.log(error);
+              }
+        }
+        getData();
+
+        
+    },[]);
+
+    const postdata= async (key)=>{
+        console.log(key)
+        console.log("runningggg")
+        const res=  await axios.post(`http://localhost:3000/customer-details/${key}`)
+    }
+ const deletedata = async(key)=>{
+    const res = await axios.post(`http://localhost:3000/customer-details/${key}`)
+ }
 
     return (
         <>
-        Welcomee
-        <Button onClick={() => setTypeList(0)}>View All Customers</Button>
-        <Button onClick={() => setTypeList(1)}> Approved List</Button>
-        <Button onClick={() => setTypeList(2)}>Pending List</Button>
+        <Sheet
+          sx={{
+            width: 1500,
+            mx: 'auto', // margin left & right
+            my: 4, // margin top & bottom
+            py: 3, // padding top & bottom
+            px: 2, // padding left & right
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            borderRadius: 'sm',
+            boxShadow: 'md',
+          }}
+          variant="outlined"
+        >
+<Table striped bordered hover>
+            <thead>
+            <tr>
+                <th>User Id</th>
+                <th> Name</th>
+                <th> Date Of Birth</th>
+                <th>Mobile Number</th>
+                <th>Adhar No</th>
+                <th> Status</th>
+            </tr>
+            </thead>
 
-       
-       {/* if (typeList==0) {
-            <CustomerList></CustomerList>
-        }else if(typeList==1) {
-            <ApprovedList></ApprovedList>
-        }else {
-            <PendingList></PendingList>
-        } */}
+            <tbody>
+            {alluser.map((val,key)=>{
+                return(
+                    <tr key = {key}>
+                        <td>{val.id}</td>
+                        <td>{val.firstName}  {val.lastName}</td>
+                        
+                        <td>{val.dob}</td>
+                        <td>{val.mobileNumber}</td>
+                        <td>${val.aadhar}</td>
+                        {!val.customer.verifiedUser && <td><Button onClick={()=> postdata(val.id)} color="success">Approve</Button>
+                        <Button onClick={()=> deletedata(val.id)}>Reject </Button> </td>}
+                        {val.customer.verifiedUser && <p>Approved</p>}
+                    </tr>
+                )
+            })}</tbody>
+            </Table>
+        </Sheet>
+        
         
         </>
     )
